913d11334448bc0095e547535e0caea5596a1065
Sancho-Panza <leo@ndrs.fr>
2016-03-07 18:25:46 +0100

    Update window flags and now there's no more warning.

--- src/window.cpp
+++ src/window.cpp
@@ -20,24 +20,23 @@ Window::Window(string w_icon_location, string w_title) {
 
 void Window::init() {
 
-  const SDL_VideoInfo* videoInfo;
-
   SDL_Init(SDL_INIT_VIDEO);
   SDL_WM_SetIcon(IMG_Load(this->icon_location.data()), NULL);
   SDL_WM_SetCaption(this->title.data(), NULL);
 
   // full screen mode
   /*
-  videoInfo = SDL_GetVideoInfo();
-  this->size.x = videoInfo->current_w;
-  this->size.y = videoInfo->current_h;
+    const SDL_VideoInfo* videoInfo;
+    videoInfo = SDL_GetVideoInfo();
+    this->size.x = videoInfo->current_w;
+    this->size.y = videoInfo->current_h;
+    this->surface = SDL_SetVideoMode(this->size.x, this->size.y, 32, SDL_FULLSCREEN|SDL_DOUBLEBUF);
   */
 
   // window mode
-  this->size.x = 800;
-  this->size.y = 600;
-
-  this->surface = SDL_SetVideoMode(this->size.x, this->size.y, 32, SDL_DOUBLEBUF);
+    this->size.x = 800;
+    this->size.y = 600;
+    this->surface = SDL_SetVideoMode(this->size.x, this->size.y, 32, SDL_DOUBLEBUF|SDL_RESIZABLE);
 
   if (this->surface == NULL) {
     error_quit(SDL_GetError());
